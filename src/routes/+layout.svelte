f61c278c87da5a81f6a88d8e7afc82f21c2bdc7a
sanciston <sanciston@gmail.com>
2026-06-16 08:07:28 +1200

    Basic Front Page in svelte

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+    import favicon from '$lib/assets/favicon.svg';
+    import '../app.css';
+
+    let { children } = $props();
+
+	const navBar = [
+		{text: "[Home]", href: "../#about"},
+		{text: "[Home]", href: "/"},
+		{text: "[Home]", href: "/"},
+	]
+</script>
+
+<svelte:head>
+    <title>Kiwi Youth Jam</title>
+    <link rel="icon" href={favicon} />
+</svelte:head>
+
+<header class="py-3 px-3">
+    <div class="py-6 bg-secondary text-light rounded-xl flex justify-between items-center px-10">
+        <h1 class="font-logo text-6xl"> KIWI YOUTH JAM </h1>
+        
+        <nav class="flex text-2xl font-display">
+			{#each navBar as navItem}
+            	<a href="{navItem.href}" class="hover:text-blue-950 px-3 transition">
+            	   {navItem.text}
+				</a>
+			{/each}
+        </nav>
+
+         <a href="/signup" class="font-display text-xl inline-block bg-black p-3 rounded-xl hover:bg-blue-950"> Sign Up </a>
+    </div>
+</header>
+
+{@render children()}
---
+++ src/routes/+page.svelte
@@ -0,0 +1,61 @@
+<script>
+
+	import favicon from '$lib/assets/favicon.svg'; 
+
+	const iconPositions = [
+		{ top: 'top-1/4', left: 'left-1/8' },
+		{ top: 'top-1/8', left: 'left-1/4' },
+		{ top: 'top-3/4', left: 'left-1/8' },
+		{ bottom: 'bottom-0', left: 'left-1/4' },
+		{ top: 'top-1/4', right: 'right-1/8' },
+		{ top: 'top-1/8', right: 'right-1/4' },
+		{ top: 'top-3/4', right: 'right-1/8' },
+		{ bottom: 'bottom-0', right: 'right-1/4' }
+	];
+
+	function handleSignUp() {
+        //TODO: Handle Sign UP
+	}
+</script>
+
+<style>
+    @reference "../app.css";
+    :global(body) {
+        @apply bg-light;
+    }
+    html {
+        scroll-behavior: smooth;
+    }
+</style>
+
+<div class="relative flex justify-center h-[60vh] mb-20">
+	<div class="flex flex-col justify-center items-center absolute top-[40%] text-center z-10">
+		<h1 class="text-9xl font-logo">KIWI YOUTH JAM</h1>
+		<h1 class="text-2xl font-display mb-10 max-w-2xl">
+			Proudly building and supporting the next generation of New Zealand game developers.
+		</h1>
+		
+		<button 
+			on:click={handleSignUp}
+			class="font-display text-2xl text-white inline-block bg-black p-6 m-15 rounded-xl hover:bg-blue-950 hover:cursor-pointer transition-colors">
+			Sign Up
+		</button>
+	</div>
+
+	{#each iconPositions as pos}
+		<img 
+			src={favicon} 
+			alt="Kiwi Youth Jam Icon"
+			height="100" 
+			width="100" 
+			class="absolute {pos.top || ''} {pos.bottom || ''} {pos.left || ''} {pos.right || ''}"
+		/>
+	{/each}
+</div>
+
+
+<section id="about" class="flex justify-center">
+    <h1 class="text-5xl font-display mb-10 max-w-2xl">
+        What is Kiwi Youth Jam
+    </h1>
+</section>
